e03bff1d7d0ed055b3883e2b354b9a1f1da5af99
SiEncan <adnanalvitomustaqim@gmail.com>
2024-11-24 10:41:00 +0700

    redesign qr code

--- lib/screen/qr/generateQr.dart
+++ lib/screen/qr/generateQr.dart
@@ -11,14 +11,98 @@ class UserQRCode extends StatelessWidget {
     String userId = authService.getUserId();
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My QR Code"),
+        elevation: 0,
+        backgroundColor: Colors.blue[400],
+        title: const Text(
+          "My QR Code",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
       ),
+      backgroundColor: Colors.blue[400],
       body: Center(
-        child: PrettyQr(
-          size: 200.0,
-          data: userId,
-          errorCorrectLevel: QrErrorCorrectLevel.M,
-          roundEdges: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 20,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "Scan Me!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: Colors.blue[100]!,
+                            width: 2,
+                          ),
+                        ),
+                        child: PrettyQr(
+                          size: 250,
+                          data: userId,
+                          errorCorrectLevel: QrErrorCorrectLevel.H,
+                          roundEdges: true,
+                          elementColor: Colors.blue[800]!,
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Image.asset(
+                          'img/T-Cash_Logo.png',
+                          width: 60,
+                          height: 60,
+                          colorBlendMode: BlendMode.hardLight,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              "Show this QR code to receive payment",
+              style: TextStyle(
+                color: Colors.blue[100],
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
         ),
       ),
     );
