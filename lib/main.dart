81cf8b4e43280b786cd97297bbab77628e5d818d
Henokh <christianoslehenokhwilliam14@gmail.com>
2024-11-29 18:44:37 +0700

    update activity page

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // Menambahkan background biru muda pada seluruh aplikasi
-        scaffoldBackgroundColor:
-            Color(0xFFA6D8FF), // Warna biru muda seperti DANA
-      ),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFA6D8FF), // Warna biru muda untuk splash screen
-      body: Center(
-        child: Text(
-          'Splash Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
